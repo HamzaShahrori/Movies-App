6a2325944d3a8ec1d89842b8110beb8ff20b21f9
HamzaShahrori <hamza.shahrorih@gmail.com>
2022-05-23 17:12:28 +0300

    modified App.js

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 import { Routes, Route } from "react-router-dom";
 
-import Home from './component/Home/Home'
-import MovieDetails from './component/MovieDetails/MovieDetails'
+import Home from "./component/Home/Home";
+import NavBar from "./component/NavBar/NavBar";
+import Favorite from "./component/Favorite/Favorite";
+
+import MovieDetails from "./component/MovieDetails/MovieDetails";
 function App() {
-  
-  return (<>
-    <div className="App">
-     <h1>movies app</h1>
-    </div>
-
-    <Routes>  
-
-      <Route path='/' element={ <Home/>}/>
-      <Route path='/movie/:id' element={<MovieDetails/>} />
-    </Routes>
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/favorite" element={<Favorite />} />
+      </Routes>
     </>
   );
 }
